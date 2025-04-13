a1a9740c5f5e725728e232bf9477b337adda5c30
caVenikk <lilnikky@mail.ru>
2025-04-13 19:24:43 +0300

    Test CI/CD commit

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,42 +1,35 @@
 <template>
-  <v-app>
-    <v-app-bar
-      color="primary"
-      app
-    >
-      <span class="app-title">Анализ производительности статических анализаторов кода</span>
-      <v-spacer />
-      <v-btn
-        icon
-        href="https://github.com/caVenikk/ITMO_nir_4"
-        target="_blank"
-      >
-        <v-icon>mdi-github</v-icon>
-      </v-btn>
-    </v-app-bar>
+    <v-app>
+        <v-app-bar color="primary" app>
+            <span class="app-title">Анализ производительности статических анализаторов кода 111</span>
+            <v-spacer />
+            <v-btn icon href="https://github.com/caVenikk/ITMO_nir_4" target="_blank">
+                <v-icon>mdi-github</v-icon>
+            </v-btn>
+        </v-app-bar>
 
-    <v-main>
-      <router-view />
-    </v-main>
-  </v-app>
+        <v-main>
+            <router-view />
+        </v-main>
+    </v-app>
 </template>
 
 <script lang="ts" setup>
-    //
+//
 </script>
 
 <style lang="scss">
-    html,
-    body {
-        overflow-y: auto;
-    }
+html,
+body {
+    overflow-y: auto;
+}
 
-    .app-title {
-        margin-left: 20px;
-        font-size: 1.25rem;
-        font-weight: 400;
-        letter-spacing: 0;
-        line-height: 1.75rem;
-        text-transform: none;
-    }
+.app-title {
+    margin-left: 20px;
+    font-size: 1.25rem;
+    font-weight: 400;
+    letter-spacing: 0;
+    line-height: 1.75rem;
+    text-transform: none;
+}
 </style>
